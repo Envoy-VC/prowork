74fad7bb1aae4f0b20282a988e06ba2f9e229965
Vedant Chainani <vedantchainani1084@gmail.com>
2023-12-09 03:36:36 +0530

    add navbar

--- app/src/components/common/navbar/index.tsx
+++ app/src/components/common/navbar/index.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
+import clsx from 'clsx';
+import Link from 'next/link';
 import { ConnectWallet } from '@thirdweb-dev/react';
+import { usePathname } from 'next/navigation';
 
-// Icons
-import { PiAirplaneTakeoffDuotone } from 'react-icons/pi';
+const NavLinks = [
+	{
+		name: 'for customers',
+		href: '/',
+	},
+	{
+		name: 'for constructors',
+		href: '/work',
+	},
+];
 
 const Navbar = () => {
+	const pathname = usePathname();
 	return (
 		<div className='p-4 px-6'>
 			<div className='flex flex-row items-center justify-between'>
 				<div className='flex flex-row items-center gap-2'>
-					<PiAirplaneTakeoffDuotone className='text-4xl text-blue-500' />
-					<div className='hidden text-2xl font-bold sm:flex'>ProWork</div>
+					<div className='hidden text-xl font-semibold sm:flex'>ProWork</div>
+				</div>
+				<div className='flex flex-row items-center gap-4'>
+					{NavLinks.map((link, index) => (
+						<Link href={link.href} key={index}>
+							<div className='group relative overflow-x-hidden'>
+								<span
+									className={clsx(
+										'text-xl',
+										pathname === link.href ? 'font-bold' : 'font-normal'
+									)}
+								>
+									{link.name}
+								</span>
+								<div
+									className={clsx(
+										'absolute bottom-0 h-[3px] w-full bg-black duration-500 ease-out group-hover:translate-x-0',
+										pathname === link.href ? 'translate-x-0' : 'translate-x-[-102%]'
+									)}
+								></div>
+							</div>
+						</Link>
+					))}
 				</div>
 				<div className='flex flex-row items-center gap-2'>
 					<ConnectWallet
-						btnTitle={'Connect'}
+						btnTitle='get started'
 						modalTitle={'Start using ProWork'}
 						modalSize={'wide'}
 						welcomeScreen={{
